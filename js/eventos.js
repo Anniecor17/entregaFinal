ef5c68cb32bc73026fcb0113d6c17e298283e307
tinchillo <tinchillo@gmail.com>
2023-02-25 15:37:47 -0300

    Entrega final

--- js/eventos.js
+++ js/eventos.js
@@ -1,1 +1,25 @@
+// Alternar entre modo claro y modo oscuro
 
+document.getElementById("button").addEventListener("click", function () {
+  document.body.style.backgroundColor = "#334140";
+  document.body.style.color = "papayawhip";
+  document.getElementById("footerBg").style.backgroundColor = "#667363";
+  document.getElementById("button").innerText = "Modo claro";
+
+  document.getElementById("button").addEventListener("click", function () {
+    location.reload();//Recarga la pagina. 
+  })
+});
+
+
+// Codigo para cambiar de nombre al titulo de la pagina cuando el usuario cambia a otra pagina.
+let tituloPrevio = document.title
+
+window.addEventListener("blur", function(){
+    tituloPrevio = document.title
+    document.title = "No te vayas, contratame!";
+})
+
+window.addEventListener("focus", function(){
+ document.title = tituloPrevio;
+})
